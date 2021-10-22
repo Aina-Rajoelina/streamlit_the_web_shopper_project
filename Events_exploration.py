bcba8436848490283114ec9540a5d4bb2f989ce5
Aina-Rajoelina <a.rajoelina@tbs-education.org>
2021-10-23 00:35:38 +0200

    Update Events_exploration.py

--- Events_exploration.py
+++ Events_exploration.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.write(print('ok'))
+#chargement du fichier category_tree
+events = pd.read_csv('C:\\Users\\Lenovo\\Documents\\DATA SCIENTEST\\PROJET E COMMERCE\\events.csv')
+
+#Aperçu des 20 premières lignes
+st.write(events.head())
